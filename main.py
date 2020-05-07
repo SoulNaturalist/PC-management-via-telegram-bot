b4c0616b2253dc5057b2a874f78dbd103b1f1508
Saywex89 <58743402+Saywex89@users.noreply.github.com>
2020-05-07 19:37:43 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,53 @@
+import random,telebot,os,webbrowser,pyautogui,time
+from PIL import Image, ImageGrab
+
+
+
+mytoken = '1176082537:AAGYLDab5uXofMVK61JFQnVvhH4w2mQAlLg'
+
+bot = telebot.TeleBot(mytoken)
+
+
+mainkeyboard = telebot.types.ReplyKeyboardMarkup(True, True)
+
+mainkeyboard.row('/off','/open','/screen')
+
+
+
+
+@bot.message_handler(content_types=['text'])
+def commands(message):
+        if message.text == '/off':
+                bot.send_message(message.chat.id,'Компьютер будет выключен!')
+                os.system('shutdown -s')
+
+
+        if message.text == '/open':
+                bot.register_next_step_handler(message,get_url)
+                bot.send_message(message.chat.id,'Отправьте ссылку!')
+                
+
+        if message.text == '/screen':
+                try:  
+                        os.remove("screenshot.png")
+                except:
+                        bot.send_message(message.chat.id,'Делаю скриншот')
+                        screen = pyautogui.screenshot('screenshot.png')
+                        screen = open('screenshot.png', 'rb')
+                        bot.send_photo(message.chat.id, screen)
+                
+
+
+
+
+def get_url(message):
+        global url
+        url = message.text
+        webbrowser.open_new(url)
+        bot.send_message(message.chat.id,'Ссылка открыта!')
+
+
+try:
+        bot.polling(none_stop=True, interval=0)
+except:
+        pass
